85e53e76b0794573cd88b809d34969cb162e024a
joshi1999 <joshi1999@yahoo.de>
2023-04-23 12:05:36 +0200

    Adding waterfall template + tasks

---
+++ src/main/java/de/greensurvivors/greenplugin/bungee/GreenPlugin.java
@@ -0,0 +1,14 @@
+package de.greensurvivors.greenplugin.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class GreenPlugin extends Plugin {
+
+  @Override
+  public void onEnable() {
+  }
+
+  @Override
+  public void onDisable() {
+  }
+}
