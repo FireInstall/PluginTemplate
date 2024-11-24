ab38b64dfebd2a41bad66e3c73a4c93a901d7503
FireInstall <feuerreiter@greensurvivors.de>
2024-11-24 21:08:18 +0100

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,20 @@
-//import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
-
 plugins {
     `java-library`
     //java
-    id("io.papermc.paperweight.userdev") version "1.7.1"
-    id("xyz.jpenilla.run-paper") version "2.3.0" // Adds runServer and runMojangMappedServer tasks for testing
-    id("xyz.jpenilla.run-waterfall") version "2.3.0" // Adds runWaterfallServer task for testing
+    id("io.papermc.paperweight.userdev") version "1.7.3"
+    id("xyz.jpenilla.run-paper") version "2.3.1" // Adds runServer and runMojangMappedServer tasks for testing
 }
 
 group = "de.greensurvivors"
 version = "1.0.0-SNAPSHOT"
 description = "A Greensurvivors Plugin "
-// this is the minecraft major version.
-val mainMCVersion by extra("1.21")
-// this is the mc major and subversion like 1.21.1
-val mcVersion by extra("$mainMCVersion")
+val mcVersion by extra("1.21.3")
 
 // we only work with paper and downstream!
 paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
 
 java {
-    // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
+    // Configure the java toolchain. This allows gradle to auto-provision JDK 21 on systems that only have JDK 8 installed for example.
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
@@ -35,8 +29,6 @@ repositories {
 
 dependencies {
     paperweight.paperDevBundle("$mcVersion-R0.1-SNAPSHOT")
-    // Waterfall
-    compileOnly("io.github.waterfallmc:waterfall-api:$mainMCVersion-R0.1-SNAPSHOT")
 }
 
 tasks {
@@ -51,12 +43,8 @@ tasks {
     processResources {
         filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
 
-        filesMatching(listOf("**/plugin.yml", "**/bungee.yml")) {
-            expand(project.properties)
-        }
-    }
-
-    runWaterfall {
-        waterfallVersion(mainMCVersion)
+        expand("version" to project.version,
+            "description" to project.description,
+            "apiVersion" to mcVersion)
     }
 }
